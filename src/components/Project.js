4c169aeeef6fc0e7aa5e2f491c24a685aa62bdd9
BT616 <110855674+BT616@users.noreply.github.com>
2022-11-14 21:13:34 -0800

    new project, fantastic  ecommerce

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,22 @@
 import React from "react";
 import project from './images/project.png'
 import first from './images/first.png'
+import ecommerce from './images/ecommerce.png'
 import passwordgen from './images/passwordgen.png'
 
 
 function Project() {
     return <div>
-        <h1 className="text-4xl text-center text-teal-600">Welcome to the Projects Page</h1>
-        <div className="container mx-auto max-w-lg shadow-lg rounded-xl  overflow-hidden bg-gray-100 ">
+        <h1 className="text-5xl text-center text-teal-600 pb-10">Welcome to the Projects Page</h1>
+        <div className="container mx-auto max-w-lg shadow-lg rounded-xl overflow-hidden bg-gray-100 ">
+
+            <img alt='ecommerce project' src={ecommerce} />
+            <h1 className="text-xl font-medium hover:underline text-center py-2 hover:text-sky-400"><a href="https://github.com/hale-bopp97/fantastic_ecomerce">Fantastic-eCommerce</a></h1>
+            <p className=" text-gray-600 px-10 py-2">An ecommerce site where you can purchase stickers, monitors, keyboards and other desktop items. Created using the Next.js
+                framework, Mongoose, Apollow-Server, Tailwind for style,Magic-Link for passwordless login and </p>
+        </div>
+
+        <div className="container mx-auto max-w-lg shadow-lg rounded-xl  overflow-hidden bg-gray-100 mt-10">
 
             <img alt='Project' src={project} />
             <h1 className="text-xl font-medium hover:underline text-center py-2 hover:text-sky-400"><a href="https://rubendc91.github.io/NutriTeam/">NutriTeam</a></h1>
@@ -24,8 +33,8 @@ function Project() {
             <h1 className="text-xl font-medium hover:underline text-center py-2 hover:text-sky-400 pd-10"><a href="https://bt616.github.io/password-generator/">Random Password Generator</a></h1>
             <p className=" text-gray-600 px-10 py-2">Random Password generator with prompts to select the criteria. After selecting criteria, you select the red generate button and a randomly selected password is presented in the box.</p>
         </div>
-        
+
     </div>
-   
+
 }
 export default Project;
