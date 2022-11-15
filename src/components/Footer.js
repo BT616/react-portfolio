7845ab236b5b972bcb30f04a3c473cfd56909d3c
BT616 <110855674+BT616@users.noreply.github.com>
2022-11-14 20:57:28 -0800

    footer name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ function Footer(){
     return (
 
         <footer className = 'fixed bottom-0 bg-gradient-to-r from-gray-400  to-red-400 text-center'>
-            <h1>2022 React-App </h1>
-            <p>Created by BT616</p>
+            <h1>2022 React-Portfolio </h1>
         </footer>
         
     )
